19d730f64831e9b4eaabe89f8e622d9feb2e476d
Reza Farhang <rezafarhangoa@gmail.com>
2025-12-22 13:41:21 +0330

    Add user and sms consts

---
+++ core/consts.py
@@ -0,0 +1,14 @@
+class SMSStatus:
+    PENDING = "pending"
+    SENT = "sent"
+    FAILED = "failed"
+
+
+class SMSType:
+    REGULAR = 1
+    EXPRESS = 2
+
+
+class QueueName:
+    EXPRESS = "express"
+    REGULAR = "regular"
